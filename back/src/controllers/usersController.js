6836a7cc49064f2e5c0fe76b8446750a829d0753
dorian-palace <dorian.palace@laplateforme.io>
2023-01-04 14:36:59 +0100

    [BACK/01] Inscription avec les vérifications en base de donnée pour le login/email et hashpassword

--- back/src/controllers/usersController.js
+++ back/src/controllers/usersController.js
@@ -1,8 +1,9 @@
 const db = require('../../database');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 var express = require('express');
-// const jwt = require('jsonwebtoken')
-// const app = express();
+const jwt = require('jsonwebtoken');
+const { config } = require('../../database');
+const app = express();
 
 // app.use(express.json());
 
@@ -18,6 +19,64 @@ const getUsers = (req, res) => {
 	})
 }
 
+const registerUsers = async (req, res) => {
+	const { login, password, email } = req.body;
+	// Champs envoyer dans la requête
+
+	try {
+		//Verification champs renseigné
+		if (login == null || password == null || email == null){
+            return res.status(400).json({'error': 'missing params'});
+        } else {		
+		//Vérification du login disponnible en base de donnée
+		db.query("SELECT * FROM users WHERE login = '"+ login +"'", async (err, response) => {
+
+			if(response.length > 0) {
+				//Si le login existe déjà en base de donnée on return l'erreur
+				return res.status(400).json({'error': 'Login not valid'});
+			}
+
+			//Vérification si le mail existe en base de donnée
+			db.query("SELECT * FROM users WHERE email = '" + email +"' ", async (err, response) => {
+
+				if (response.length > 0) {
+					//Si l'email existe déjà en base de donnée on return l'erreur
+					return res.status(400).json({'error': 'email alreaddy used'});
+				}
+
+			// Hash password
+			const salt = await bcrypt.genSalt()
+			const hash =  await bcrypt.hash(password, salt);
+					
+					// Insert user into database
+			db.query(
+				`INSERT INTO users (login, password, email, id_role) VALUES ("${login}", "${hash}", "${email}", 1)`, (err, response) => {
+					if(err) {
+						res.status(500).json({
+							status: false,
+							message: 'There was a problem with the query.'
+						});
+
+					} else {
+
+						// send the JWT to the client
+						res.status(200).json({
+							status: true,
+							message: 'Inscription valider'
+						});
+					}
+				});
+			});
+		})
+
+		}
+	}catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
+}
+
+
 module.exports = {
-	getUsers,
+	getUsers, registerUsers,
 }
--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var {
-	getUsers, 
+	getUsers, registerUsers,
 } = require('../controllers/usersController')
 
 // Une route qui retourne tous les utilisateurs dans une liste contenant les champs prenom et nom.
 router.get('/', getUsers);
+
+// Une route qui inscrit les utilisateurs BACK/01-inscription
+router.post('/inscription', registerUsers);
 
 module.exports = router;
