1f44065d8373dd26c5195cb66875397655971a4c
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2023-01-05 19:51:58 +0100

    gestion de conflits sur index.js au pull de sprint/1

--- back/index.js
+++ back/index.js
@@ -17,10 +17,6 @@ app.use('/users', users);
 
 var participants = require('./src/routes/participants');
 app.use('/participants', participants);
-<<<<<<< HEAD
-=======
-
->>>>>>> BACK/19-get-rooms-of-user
 
 // Start server
 app.listen(3000, () => {
