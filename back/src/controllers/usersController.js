5651095b91e3a29936b8a8c5fd937d3ec953db84
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2023-01-05 14:45:21 +0100

    route et requete ok/ -> check à effectuer avec l'accès aux middleWare verifyToken et checkAdmin

--- back/src/controllers/usersController.js
+++ back/src/controllers/usersController.js
@@ -7,6 +7,8 @@ var express = require('express');
 // app.use(express.json());
 
 const getUsers = (req, res) => {
+
+	
 	const sql = 'SELECT `login` FROM users'
 	db.query(sql, function(error, data){
 		if (error) {
@@ -14,10 +16,28 @@ const getUsers = (req, res) => {
 		}
 		else {
 			res.send(data);
+			console.log(data)
 		}
 	})
+	
 }
 
+// permet à l'admin de modifier le rôle et le login d'un user.
+
+const adminUpdateUser = (req, res) => {
+
+	
+	const sql = 'UPDATE  users SET login = ?, id_role = ? WHERE id = ?'
+	db.query(sql, [req.body.login, req.body.id_role, req.params.id] , function(error, data){
+
+		if(error){throw error;}
+		else{res.send(data).status(204);}
+		
+	})
+
+}
+
+
 module.exports = {
-	getUsers,
+	getUsers, adminUpdateUser
 }
--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var {
-	getUsers, 
+var { getUsers, adminUpdateUser
 } = require('../controllers/usersController')
 
 // Une route qui retourne tous les utilisateurs dans une liste contenant les champs prenom et nom.
 router.get('/', getUsers);
+router.patch('/admin/update/:id', adminUpdateUser);
 
 module.exports = router;
