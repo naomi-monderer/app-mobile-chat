76ee3548fb68e05d4c9c9665767386f9dbbc5543
Laura Savickaite <laura.savickaite@laplateforme.io>
2023-01-05 16:17:27 +0100

    [BACK/07] verification and get users details

---
+++ back/src/middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const JWT_SIGN_SECRET = 'oaziehiozaaoi8756123hiauzdi29';
+const db = require('../../database');
+const bcrypt = require('bcrypt');
+var express = require('express');
+const app = express();
+
+
+exports.signIn = (req, res, next) => {
+
+	var signInOptions = {
+		expireIn: "7d",
+		algorithm:  "HS256"
+	}
+	const tokenToUse = req.headers; 
+	const token = tokenToUse.authorization.split(' ')[1]
+	
+	
+	if(!tokenToUse.hasOwnProperty("authorization")) res.status(401).json({ message: "Authorization not found"})
+	try {
+			const mySecret = "mysecret";
+			const decoded = jwt.verify(token, mySecret, signInOptions);
+
+			req.user = decoded;
+
+		} catch (err) {
+			return res.status(401).send("Invalid Token");
+		}
+		return next();
+};
--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var {signIn} = require('../middlewares/auth');
 var {
 	getUsers, 
+	authUsers, 
+	connectedUser,
+	getUserDetails
 } = require('../controllers/usersController')
+
+// route [BACK/02 verif token and secure route for connected users]
+router.get('/signin', signIn, connectedUser);
+
+//route [BACK/02 connexion de l'user et attribution du token]
+router.post('/auth',  authUsers)
 
 // Une route qui retourne tous les utilisateurs dans une liste contenant les champs prenom et nom.
 router.get('/', getUsers);
+
+//route [BACK/07] get the details from 1 user
+router.get('/:userId', signIn ,getUserDetails);
 
 module.exports = router;
