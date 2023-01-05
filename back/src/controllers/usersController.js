cacc9c6d6ef2cd9060e166e4d65f4c77edfa3362
dorian-palace <dorian.palace@laplateforme.io>
2023-01-05 15:59:57 +0100

    #

--- back/src/controllers/usersController.js
+++ back/src/controllers/usersController.js
@@ -26,6 +26,7 @@ const updateUser = async (req, res) => {
 	 * 
 	 */
 	const { login, email, password, confPassword } = req.body;
+	console.log(password)
 	try {
 	
 		if (login == null || email == null){
