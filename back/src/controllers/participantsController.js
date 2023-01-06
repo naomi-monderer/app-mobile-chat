8ec1e9855f6ab492e894f8d2695a81e008f04e9c
Laura Savickaite <laura.savickaite@laplateforme.io>
2023-01-06 12:17:14 +0100

    sprint/1 BACK/04 corrected

--- back/src/controllers/participantsController.js
+++ back/src/controllers/participantsController.js
@@ -8,8 +8,7 @@ var express = require('express');
 
 const getParticipants = (req, res) => {
 	// récupérer du front lorsque sur une chatroom et click sur (view les utilisateurs) ++ potentiellement en back get tous les id des groupes et les mettre en front en caché ???
-
-	if(req.user.id === req.params.roomId) {
+	if(req.user.id_rooms.includes(req.params.roomId)) {
 		const sql = `SELECT login FROM users INNER JOIN participants ON users.id = id_user WHERE id_room = ${req.params.roomId}`
 		db.query(sql, function(error, data){
 			if (error) {
@@ -27,7 +26,6 @@ const getParticipants = (req, res) => {
 
 const deleteUser = (req, res) => {
 	// récupérer et afficher toutes les rooms dispo à l'user s'il y est inscrit sinon rien afficher
-
 	if(req.user.id === req.params.userId) {
 		const sql = `DELETE FROM participants WHERE id_room = ${req.params.roomId} AND id_user = ${req.params.userId}`
 		db.query(sql, function(error, data){
