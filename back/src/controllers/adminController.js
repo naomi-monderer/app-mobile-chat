6f0c09c9293e2b5c40c2b76fad4ef95661affd1d
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2023-01-06 10:34:25 +0100

    test

--- back/src/controllers/adminController.js
+++ back/src/controllers/adminController.js
@@ -60,15 +60,6 @@ const adminUpdateUser = (req, res) => {
 // permet à l'admin de modifier le rôle et le login d'un user.
 const adminUpdateRole = (req, res) => {
 
-	/*
-	- route accessible qu'à L'ADMIN  (ok)
-- vérifier que l'utilisateur est l'admin.(ok)
-- vérifier que l'user qu'il veut ban est BIEN LE BON USER (check id)
-- vérifier que dans la BDD il est retiré de TOUS ses groupes
-- vérifier que son role est passé à 0 
-- vérifier que PARCE QUE son role est passé à 0, en front, il ne peut se réinscrire dans un groupe (l'option lui est enlevé).
-- vérifier qu'il ne peut plus envoyer de message dans le chat global (socket io).
-	*/
 	const sql = 'UPDATE users SET id_role = ? WHERE id = ?'
 
 	db.query(sql, [req.body.id_role, req.params.id] , function(error, data){
