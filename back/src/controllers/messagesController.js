e6799981e7a904aab87fa2e9b4e074b1aea537bd
Laura Savickaite <laura.savickaite@laplateforme.io>
2023-01-09 17:08:17 +0100

    sprint/1 ajout id getmessageglobalchat

--- back/src/controllers/messagesController.js
+++ back/src/controllers/messagesController.js
@@ -102,7 +102,7 @@ const specificChat = (req, res) => {
 }
 
 const getMessagesinGlobalChat = (req, res) => {
-	const sql = `SELECT content, created_at, users.login FROM messages INNER JOIN users ON id_user = users.id WHERE id_room = 0`
+	const sql = `SELECT id, content, created_at, users.login FROM messages INNER JOIN users ON id_user = users.id WHERE id_room = 0`
 
 	db.query(sql, function (err, data) {
 		if (err) throw err;
