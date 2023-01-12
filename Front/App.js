3a260c7807ccacf9529eee56dd9ec48eff2536e8
dorian-palace <dorian.palace@laplateforme.io>
2023-01-12 17:48:35 +0100

    Architecture front

--- Front/App.js
+++ Front/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>GaGaga</Text>
-      <Text>Tony Ta gueule</Text>
+      <Text>Tony</Text>
       <StatusBar style="auto" />
     </View>
   );
