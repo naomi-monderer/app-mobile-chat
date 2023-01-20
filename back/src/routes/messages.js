eaf13534cd3c6f4f376b89859850aa2fe8aa1af9
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2023-01-20 13:34:02 +0100

    ajout des modif dans messages.js et messagesController.js ( le temps du secure store pour voir si axios fonctionne)

--- back/src/routes/messages.js
+++ back/src/routes/messages.js
@@ -14,7 +14,7 @@ var {
 router.post('/', signIn, postMessage);
 
 //[BACK/10]: Une route qui poste sur un chat spé.
-router.post('/:roomId', signIn, postMessageinChat);
+router.post('/:roomId', postMessageinChat);
 
 //[BACK/18-delete-own-messafe]: Une route qui delete un message.
 router.delete('/delete/:messageId', signIn, deleteMessage);
