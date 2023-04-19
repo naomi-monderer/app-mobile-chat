13888557593c33cb46cca0820aa7cdd41fd48c29
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2023-04-19 16:06:31 +0200

    resolve merge conflicts

--- back/index.js
+++ back/index.js
@@ -44,7 +44,7 @@ const cors = require('cors');
 const corsOptions ={
     origin:'http://localhost:19006', 
     origin:'http://localhost',
-    origin:'http://localhost:8888',
+    origin:'http://localhost:8881',
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
