fbae42d7de017cd593935db58abdaca19742e7e1
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2023-04-20 13:43:47 +0200

    commit before switch branch

--- back/src/middlewares/isAdmin.js
+++ back/src/middlewares/isAdmin.js
@@ -2,21 +2,11 @@ var express = require('express');
 
 
 exports.isAdmin = (req, res, next) => {
-<<<<<<< HEAD
 	console.log('req', req.body.id_role_admin)
     if (req.body.id_role_admin === 2) {
     	return next();
 	}else {
 		return res.status(400).json({ message: "You are not an administrator" });
-=======
-	const { idAdmin, username, id_role } = req.user;
-	const userInfo = { idAdmin, username, id_role };
-	if (id_role === 2) {
-		req.userInfo = userInfo
-	  return [next()];
-	} else {
-	  return res.status(400).json({ message: "You are not an administrator", userInfo });
->>>>>>> FRONT/17-2_fix_issues
 	}
   }
   
