e587e689cd76a1bfdd778dfdbfe21cbc9317ab71
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2023-05-09 13:05:58 +0200

    tri par id desc dans la requete getAllRooms

--- back/src/controllers/roomsController.js
+++ back/src/controllers/roomsController.js
@@ -2,7 +2,7 @@ const db = require('../../database');
 var express = require('express');
 
 const getAllRooms = (req, res) => {
-	const sql = 'SELECT * FROM rooms';
+	const sql = 'SELECT * FROM rooms ORDER BY id DESC';
 	db.query(sql, function(error,data){
 		console.log('req +++++' + req.query);
 		if(error)throw error;
