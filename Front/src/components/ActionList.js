4ba6ea0184256a968262f5fadb3a660a19c97cc7
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2023-05-10 13:26:07 +0200

    add action list menu component for profil

---
+++ Front/src/components/ActionList.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import axios from 'axios';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import { API } from '../constant/constant';
+import jwt_decode from "jwt-decode";
+// import { getUserInfos, logOut} from "../api/auth";
+
+const ActionList = ({ handleModifyProfile, logOut, handleDeleteAccount }) => {
+
+  
+
+    
+  const data = [
+    { id: '1', title: 'Modify Profile', onPress: handleModifyProfile },
+    { id: '2', title: 'Sign Out', onPress: logOut  },
+    { id: '3', title: 'Delete Account', onPress: handleDeleteAccount },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={item.onPress}  style={styles.item}>
+            <Text style={styles.itemText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
+    </View>
+  );
+};
+
+const styles = {
+  container: {
+    height: 200,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 16,
+    marginTop: 50,
+  },
+  item: {
+    height: 40,
+    justifyContent: 'center',
+  },
+  itemText: {
+    fontSize: 18,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#ccc',
+    marginVertical: 8,
+  },
+};
+
+export default ActionList;
