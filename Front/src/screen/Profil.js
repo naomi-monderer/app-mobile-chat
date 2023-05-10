917cc307fb133ec3c70cc4a89fb09f7a51af2a5e
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2023-05-10 12:01:33 +0200

    get data from back api & secure store ok

--- Front/src/screen/Profil.js
+++ Front/src/screen/Profil.js
@@ -1,17 +1,52 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import LogoutButton from '../components/LogoutButton';
+import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
+import { API } from '../constant/constant';
+import jwt_decode from "jwt-decode";
 
 function Profil() {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token1')
+      .then(token => {
+        const decoded = jwt_decode(token);
+      
+        setUserData(decoded)
+        console.log(userData,'ljneirhfiqlrj');
+        const url = `${API}/users/details/${userData?.id}`;
+
+        axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+          .then(response => {
+            console.log(response.data);
+            setUserData(response.data);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>profil</Text>
-      <LogoutButton />
+      <Text>Profile</Text>
+      {userData && (
+        <>
+          <Text>Login: {userData.login}</Text>
+          <Text>Email: {userData.email}</Text>
+          <Text>Rooms: {userData.id_rooms}</Text>
+        </>
+      )}
     </View>
   );
 }
-
 
 export default Profil;
